a0b42b87fde9a1810bf9ba4bbbc470a6a6e50815
Yatharth Arora <yathartharora1999@gmail.com>
2022-04-09 13:53:58 +0530

    API deployment

--- apprun.py
+++ apprun.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-from apicall import Todo
+from resources.todo import Todo
 
 app = Flask(__name__)
 api = Api(app)
 
-# api.add_resource(Todo, "/todo/<int:id>")
-api.add_resource(Todo, "/data")
+api.add_resource(Todo, "/todo/<int:id>")
 
 if __name__ == "__main__":
   app.run()
